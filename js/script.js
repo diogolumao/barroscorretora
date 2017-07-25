0bb3f1789f49e8a0b2f581e219803521e1a5ed2e
Diogo <diogolumao@gmail.com>
2017-07-25 10:56:39 -0300

    envio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+  $('#modal1').modal('open');
+  $('#modal1').modal('close');
+});
+ $(document).ready(function(){
+  $('.parallax').parallax();
+});
+jQuery(function($){
+   $("#telefone").mask("(99) 9999-9999");
+
+});
